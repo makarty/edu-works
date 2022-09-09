b0742ed71d5cfe15f84a6d8099c3b540e1c31abd
Artem M <artmakarov2002@mail.ru>
2022-09-09 12:49:01 +0700

    Начало 1 работы по ИБ

---
+++ is/1practical/main.py
@@ -0,0 +1,30 @@
+def mirabeau_cipher():
+    pass
+
+
+def create_an_alphabet():
+    polybius_square = []
+    cur_char = ord("а")
+    for i in range(6):
+        group = []
+        for j in range(6):
+            if i == 1 and j == 0:
+                group.append("ё")
+                continue
+            if i == 5 and j > 2:
+                group.append("null")
+                continue
+            group.append(chr(cur_char))
+            cur_char += 1
+        polybius_square.append(group)
+
+    return polybius_square
+
+
+def main():
+    alphabet = create_an_alphabet()
+    print(alphabet)
+
+
+if __name__ == '__main__':
+    main()
