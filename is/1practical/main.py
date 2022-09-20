24a9e0aff8f835d1a5698bfcb555dbb9b21e2c30
Artem Makarov <artmakarov2002@mail.ru>
2022-09-20 21:08:29 +0700

    Main отвечает только за запуск программы

--- is/1practical/main.py
+++ is/1practical/main.py
@@ -1,68 +1,9 @@
-# Шифр Мирабо
-
 from gui import *
 from PyQt5.QtWidgets import QApplication
 import sys
 
 
-class MirabeauCipher:
-
-    def __init__(self, alphabet):
-        self.alphabet = alphabet
-
-    # Шифровка
-    def encryption(self, text: str):
-        cipher = []
-        for symbol in text:
-            if symbol == ' ':
-                cipher.append(0)
-            group_num = 1
-            for group in self.alphabet:
-                if symbol in group:
-                    encrypted_letter = group_num + (group.index(symbol) + 1) / 10
-                    cipher.append(encrypted_letter)
-                group_num += 1
-        return cipher
-
-    # Дешифровка
-    def decryption(self, cipher: list):
-        decrypted_text = ""
-        for encrypted_letter in cipher:
-            encrypted_letter *= 10
-            i = int(encrypted_letter // 10 - 1)
-            j = int(encrypted_letter % 10 - 1)
-            decrypted_text += self.alphabet[i][j]
-        return decrypted_text
-
-
-def create_an_alphabet():
-    polybius_square = []
-    cur_char = ord("а")
-    for i in range(6):
-        group = []
-        for j in range(6):
-            if i == 1 and j == 0:
-                group.append("ё")
-                continue
-            if i == 5 and j > 2:
-                group.append("null")
-                continue
-            group.append(chr(cur_char))
-            cur_char += 1
-        polybius_square.append(group)
-
-    return polybius_square
-
-
-def print_alphabet(alphabet: list):
-    for group in alphabet:
-        print(group)
-
-
 def main():
-    alphabet = create_an_alphabet()
-    print_alphabet(alphabet)
-
     app = QApplication(sys.argv)
     win = MyWindow()
     win.show()
