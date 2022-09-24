b562a77c28ae05e06ae7e5940182dbd1e317c978
Artem Makarov <artmakarov2002@mail.ru>
2022-09-24 20:57:12 +0700

    Добавлен флаг и сообщения об ошибках

--- is/1practical/gui.py
+++ is/1practical/gui.py
@@ -1,4 +1,5 @@
 from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMessageBox
 from cipher import *
 
 
@@ -12,32 +13,47 @@ class MyWindow(QtWidgets.QMainWindow):
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 300)
+        MainWindow.resize(1000, 300)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(15, 160, 200, 50))
+        self.pushButton.setGeometry(QtCore.QRect(15, 180, 200, 50))
         self.pushButton.setObjectName("pushButton")
-        self.resultLabel = QtWidgets.QLabel(self.centralwidget)
+        
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(215, 160, 200, 50))
+        self.pushButton_2.setGeometry(QtCore.QRect(215, 180, 200, 50))
         self.pushButton_2.setObjectName("pushButton_2")
+
+        self.resultLabel = QtWidgets.QLabel(self.centralwidget)
+
         self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
         self.textEdit.setGeometry(QtCore.QRect(15, 100, 400, 50))
         self.textEdit.setObjectName("textEdit")
+
         self.pushButton.clicked.connect(self.encrypt)
         self.pushButton_2.clicked.connect(self.decrypt)
+
+        self.protect = QtWidgets.QCheckBox(self.centralwidget)
+        self.protect.setGeometry(QtCore.QRect(15, 160, 400, 15))
+        self.protect.setText("Повысить криптостойкость")
+
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(15, 49, 400, 51))
         self.label.setStyleSheet("font-size: 11px")
         self.label.setObjectName("label")
+
         MainWindow.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 30))
         self.menubar.setObjectName("menubar")
+
         MainWindow.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
+
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
@@ -52,11 +68,20 @@ class Ui_MainWindow(object):
 
     def encrypt(self):
         text = self.textEdit.toPlainText()
-        if not text.isalpha():
+        rep = text.replace(' ', 'а')
+        if not rep.isalpha():
+            message = QMessageBox(self.centralwidget)
+            message.setWindowTitle("Ошибка")
+            message.setText("Недопустимые символы")
+            message.setIcon(QMessageBox.Critical)
+            message.show()
             return
         cl = MirabeauCipher()
-        enc = str(cl.encryption(text))
-        self.resultLabel.setGeometry(QtCore.QRect(430, 90, 400, 50))
+        if self.protect.isChecked():
+            enc = str(cl.encryption_2x(text))
+        else:
+            enc = str(cl.encryption(text))
+        self.resultLabel.setGeometry(QtCore.QRect(430, 90, 500, 50))
         self.resultLabel.setStyleSheet("font-size: 19px")
         self.resultLabel.setText("Результат: " + enc)
 
@@ -66,10 +91,27 @@ class Ui_MainWindow(object):
         for i in range(len(text)):
             digit = text[i].replace('.', '')
             if not digit.isdigit():
+                message = QMessageBox(self.centralwidget)
+                message.setWindowTitle("Ошибка")
+                message.setText("Недопустимые символы")
+                message.setIcon(QMessageBox.Critical)
+                message.show()
                 return
             text[i] = float(text[i])
         cl = MirabeauCipher()
-        dec = cl.decryption(text)
+        try:
+            if self.protect.isChecked():
+                dec = cl.decryption_2x(text)
+                dec = cl.decryption(dec)
+            else:
+                dec = cl.decryption(text)
+        except IndexError:
+            message = QMessageBox(self.centralwidget)
+            message.setWindowTitle("Ошибка")
+            message.setText("Введена неверная последовательность")
+            message.setIcon(QMessageBox.Critical)
+            message.show()
+            return
         self.resultLabel.setGeometry(QtCore.QRect(430, 90, 400, 50))
         self.resultLabel.setStyleSheet("font-size: 19px")
         self.resultLabel.setText("Результат: " + dec)
